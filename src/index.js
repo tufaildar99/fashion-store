b0b0d8cc8c93def11064428c7d074c6b455b5f4a
Tufail <tufaildar2016@gmail.com>
2024-07-22 10:07:45 +0530

    refactor: Update import statements and rename ProductDetails to MyAccountPersonal

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import { Checkout } from "./pages/Checkout";
+import { MyAccountPersonal } from "./pages/MyAccountPersonal";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <Checkout />
+    <MyAccountPersonal />
   </React.StrictMode>
 );
 
